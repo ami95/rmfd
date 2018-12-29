c73f421b0a90c0acaaf5f6a27f16e4d381481323
ami95 <mihaiadrian1995@gmail.com>
2018-12-29 18:27:56 +0200

    modify language/timezone/allowed_hosts in settings.py

--- rmfd/settings.py
+++ rmfd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm@u(930#^x&=i968xv*=$*bl&ms$@7zvm_%c7$tcfy^n@&j^0f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ro-ro'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Bucharest'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
