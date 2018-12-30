ff21161e66d221c366758f348f5b773aab2acefd
George Adrian Mihai <george.mihai@ad.infosys.com>
2018-12-30 16:16:55 +0100

    add tables for Post model

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Patricia este aleasa inimii mele şi o iubesc !")
+    return HttpResponse("Patricia este foarte frumoasa şi o iubesc!")
