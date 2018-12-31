c6bb21893de44330608b72ad64897194155a101e
George Adrian Mihai <george.mihai@ad.infosys.com>
2018-12-31 22:50:21 +0100

    updated httpresponse

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Patricia este foarte frumoasa şi o iubesc!")
+    return HttpResponse("Grazie di esistere Patricia!. Te iubesc :)")
